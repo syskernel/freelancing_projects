27a2588d349f356e9152f532a3e45a56c15f86cb
Anjali2192 <967anjalimishra@gmail.com>
2026-02-01 09:11:15 +0530

    Saved data in a csv

--- Project 7/datacentre.py
+++ Project 7/datacentre.py
@@ -1,10 +1,30 @@
 import asyncio
 from playwright.async_api import async_playwright
-import time
+import pandas as pd
+
+datacentre = []
 
 async def next_button(page):
     nxt_btn = await page.query_selector('//button[@data-testid="next-page-button"]')
     await nxt_btn.click()
+
+async def get_data(page):
+    items = await page.query_selector_all('//a[@class="flex flex-col gap-2 rounded border border-gray-100 p-2 hover:border-teal-300 hover:shadow-lg hover:shadow-teal-600/40"]')
+    for item in items:
+        box = await item.query_selector('//div[@class="flex flex-col gap-2"]')
+        name_l = await box.query_selector('//div[@class="text font-medium hover:text-purple"]')
+        name = (await name_l.inner_text()).strip() if name_l else 'N/A'
+
+        meta = await box.query_selector_all('//div[@class="text-xs text-gray-500"]')
+
+        provider = (await meta[0].inner_text()).strip() if len(meta) > 0 else 'N/A'
+        adress = (await meta[1].inner_text()).strip() if len(meta) > 1 else 'N/A'
+
+        datacentre.append({
+            "NAME": name,
+            "PROVIDER": provider,
+            "ADDRESSS": adress
+        })
 
 async def save_session():
     async with async_playwright() as p:
@@ -20,21 +40,7 @@ async def save_session():
         await page.goto("https://www.datacenters.com/locations/australia", timeout=60000)
         await page.wait_for_load_state("networkidle")
         await page.wait_for_selector('//div[@data-testid="container"]', timeout=10000)
-        items = await page.query_selector_all('//a[@class="flex flex-col gap-2 rounded border border-gray-100 p-2 hover:border-teal-300 hover:shadow-lg hover:shadow-teal-600/40"]')
-        for item in items:
-            box = await item.query_selector('//div[@class="flex flex-col gap-2"]')
-            name_l = await box.query_selector('//div[@class="text font-medium hover:text-purple"]')
-            name = (await name_l.inner_text()).strip() if name_l else 'N/A'
-
-            meta = await box.query_selector_all('//div[@class="text-xs text-gray-500"]')
-
-            provider = (await meta[0].inner_text()).strip() if len(meta) > 0 else 'N/A'
-            adress = (await meta[1].inner_text()).strip() if len(meta) > 1 else 'N/A'
-
-            print(f"NAME: {name}")
-            print(f"PROVIDER: {provider}")
-            print(f"ADDRESSS: {adress}")
-            time.sleep(2)
+        await get_data(page)
 
         await context.storage_state(path="C:/browser_profiles/session.json")
 
@@ -42,3 +48,7 @@ async def save_session():
         await browser.close()
 
 asyncio.run(save_session())
+
+df = pd.DataFrame(datacentre)
+df.to_csv("datacentre.csv", index=False)
+print("All details successfully saved to datacentre.csv")
