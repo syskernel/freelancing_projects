1e52d8bc60e12638f72501d322be99730025b184
Anjali2192 <967anjalimishra@gmail.com>
2026-02-01 08:56:01 +0530

    Defined function for data and next button

--- Project 7/datacentre.py
+++ Project 7/datacentre.py
@@ -2,6 +2,10 @@ import asyncio
 from playwright.async_api import async_playwright
 import time
 
+async def next_button(page):
+    nxt_btn = await page.query_selector('//button[@data-testid="next-page-button"]')
+    await nxt_btn.click()
+
 async def save_session():
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=False)
@@ -15,11 +19,22 @@ async def save_session():
 
         await page.goto("https://www.datacenters.com/locations/australia", timeout=60000)
         await page.wait_for_load_state("networkidle")
-        #await page.wait_for_selector('//div[@data-testid="container"]', timeout=10000)
-        #items = await page.query_selector_all('//a[@class="flex flex-col gap-2 rounded border border-gray-100 p-2 hover:border-teal-300 hover:shadow-lg hover:shadow-teal-600/40"]')
-        nxt_btn = await page.query_selector('//button[@data-testid="next-page-button"]')
-        await nxt_btn.click()
-        time.sleep(3)
+        await page.wait_for_selector('//div[@data-testid="container"]', timeout=10000)
+        items = await page.query_selector_all('//a[@class="flex flex-col gap-2 rounded border border-gray-100 p-2 hover:border-teal-300 hover:shadow-lg hover:shadow-teal-600/40"]')
+        for item in items:
+            box = await item.query_selector('//div[@class="flex flex-col gap-2"]')
+            name_l = await box.query_selector('//div[@class="text font-medium hover:text-purple"]')
+            name = (await name_l.inner_text()).strip() if name_l else 'N/A'
+
+            meta = await box.query_selector_all('//div[@class="text-xs text-gray-500"]')
+
+            provider = (await meta[0].inner_text()).strip() if len(meta) > 0 else 'N/A'
+            adress = (await meta[1].inner_text()).strip() if len(meta) > 1 else 'N/A'
+
+            print(f"NAME: {name}")
+            print(f"PROVIDER: {provider}")
+            print(f"ADDRESSS: {adress}")
+            time.sleep(2)
 
         await context.storage_state(path="C:/browser_profiles/session.json")
 
