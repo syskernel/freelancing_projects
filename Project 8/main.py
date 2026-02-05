e5844fa0c1760ccbe9b37e5e1bc437c14270aef0
Anjali2192 <967anjalimishra@gmail.com>
2026-02-05 18:29:20 +0530

    Initialized the browser

---
+++ Project 8/main.py
@@ -0,0 +1,31 @@
+import asyncio
+from playwright.async_api import async_playwright
+import logging
+import time
+
+async def save_session():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        context = await browser.new_context(viewport={"width":1280, "height":800})
+        page = await context.new_page()  
+        logging.info("Script started")  
+
+        await page.set_extra_http_headers({
+            "Accept-language": "en=US,en;q=0.9",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36"
+        })    
+
+        logging.info("Going to the site")
+        await page.goto("http://delcorealestate.co.delaware.pa.us/PT/search/advancedsearch.aspx?mode=advanced", timeout=60000)
+        await page.wait_for_load_state("networkidle")
+
+        agree = await page.query_selector('//button[@id="btAgree"]')
+        await agree.click()
+        time.sleep(5)
+
+        await context.storage_state(path="C:/browser_profiles/delaware.json")
+
+        await context.close()
+        await browser.close()
+
+asyncio.run(save_session())
