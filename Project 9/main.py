56d40b2567aab13eb5495c11f83559c2ec00ffa0
Anjali2192 <967anjalimishra@gmail.com>
2026-02-14 09:58:12 +0530

    Confirmed AOC Page

--- Project 9/main.py
+++ Project 9/main.py
@@ -1,11 +1,24 @@
 import asyncio
 from playwright.async_api import async_playwright
 
+async def aoc_page(active_page):
+    locator = active_page.locator("td.page_title", has_text="AOC Summary")
+    count = await locator.count()
+    if count > 0:
+        print("Confirmed: AOC page")
+        return True
+    else:
+        print("Not on AOC page")
+
+async def fetch_page(active_page):
+    pass
+
 async def save_session():
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=False)
         context = await browser.new_context(viewport={"width":1280, "height":800})
         page = await context.new_page()
+        active_page = page
 
         await page.set_extra_http_headers({
             "Accept-language": "en=US,en;q=0.9",
@@ -14,7 +27,27 @@ async def save_session():
 
         await page.goto("https://eprocure.gov.in/eprocure/app", timeout=60000)
         await page.wait_for_load_state("networkidle")
-        #await page.wait_for_selector("#tender-details")
-        await asyncio.sleep(100000)
+
+        async def handle_new_page(new_page):
+            nonlocal active_page
+            await new_page.wait_for_load_state("domcontentloaded")
+            active_page = new_page
+
+        context.on(
+            "page",
+            lambda new_page: asyncio.create_task(handle_new_page(new_page))
+        )
+
+        while True:
+            command = await asyncio.to_thread(input, "Enter command: ")
+            if command == "page url":
+                if await aoc_page(active_page) == True:
+                    print(active_page.url)
+                    print("You can Fetch data now")
+            else:
+                break
+        
+        await context.close()
+        await browser.close()
 
 asyncio.run(save_session())
